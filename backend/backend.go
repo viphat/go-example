7f2709adc81d007f2224b294de03c012c5e7e43c
Duong Vi Phat <viphat@gmail.com>
2018-08-21 23:39:38 +0900

    Initial commit

---
+++ backend/backend.go
@@ -0,0 +1,9 @@
+package backend
+import (
+  "fmt"
+)
+
+func PrintHelloWorld() {
+	fmt.Printf("Hello World\n")
+}
+
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/viphat/go-example/backend"
+)
+
+func main() {
+	backend.PrintHelloWorld()
+}
